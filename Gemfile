dbf4275f00731baf9fd5f0a706b99e19e44b8dec
Jose Luis Assadi <jlassadi@miuandes.cl>
2018-06-09 17:30:41 -0400

    cancancan implemented

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'therubyracer'
 gem 'omniauth'
 gem 'omniauth-facebook'
 
+gem 'cancancan', '~> 2.0'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   # GET /users
   # GET /users.json
   def index
     @users = User.all
+    authorize! :list, @users
   end
 
   # GET /users/1
@@ -14,6 +16,7 @@ class UsersController < ApplicationController
         wishlist: current_user.my_wishlist,
         projects: Project.where(:user_id => params[:id])
     }
+
   end
 
   # GET /users/new
